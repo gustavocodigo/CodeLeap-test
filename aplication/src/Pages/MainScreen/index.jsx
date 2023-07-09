0e3dc2751884c33b67a5ff01c8b95cd266f60c40
gustavocodigo <gilapesgus@gmail.com>
2023-07-08 23:23:58 -0300

    todo fix date comparation

--- aplication/src/Pages/MainScreen/index.jsx
+++ aplication/src/Pages/MainScreen/index.jsx
@@ -13,23 +13,59 @@ import { useSelector } from "react-redux";
 
 import loading_icon from "../../assets/loading-blue.gif"
 
-function formatElapsedTime(dateString) {
-    const date = new Date(dateString);
-    const now = new Date();
-    const diffMilliseconds = now - date;
-    const diffMinutes = Math.floor(diffMilliseconds / 1000 / 60);
 
-    if (diffMinutes < 60) {
-        return `${diffMinutes} minutes ago`;
-    } else if (diffMinutes < 1440) {
-        const diffHours = Math.floor(diffMinutes / 60);
-        return `${diffHours} hours ago`;
+import moment from 'moment';
+
+function formatElapsedTime(dateString) {
+  const now = moment();
+  const date = moment(dateString);
+  const diffDuration = moment.duration(now.diff(date));
+  const diffSeconds = diffDuration.asSeconds();
+  
+  if (diffSeconds < 60) {
+    if (diffSeconds <= 1) {
+      return `1 second ago`;
     } else {
-        const diffDays = Math.floor(diffMinutes / 1440);
-        return `${diffDays} days ago`;
+      return `${Math.floor(diffSeconds)} seconds ago`;
     }
+  } else if (diffSeconds < 3600) {
+    const diffMinutes = Math.floor(diffSeconds / 60);
+    return `${diffMinutes} minutes ago`;
+  } else if (diffSeconds < 86400) {
+    const diffHours = Math.floor(diffSeconds / 3600);
+    return `${diffHours} hours ago`;
+  } else {
+    const diffDays = Math.floor(diffSeconds / 86400);
+    return `${diffDays} days ago`;
+  }
 }
 
+  
+  
+// Exemplo de uso:
+//   const dateString = '2023-07-08T10:30:00Z'; // Data em UTC
+//   const localDateString = new Date(dateString).toLocaleString();
+
+//   console.log(localDateString); // Exibe a data convertida para o fuso horário local
+
+
+
+// {
+// 	"id": 65039,
+// 	"username": "user",
+// 	"created_datetime": "2023-07-09T02:04:37.898865Z",
+// 	"title": "3333",
+// 	"content": "4444"
+// }
+
+
+// {
+// 	"id": 65040,
+// 	"username": "user",
+// 	"created_datetime": "2023-07-09T02:04:58.207805Z",
+// 	"title": "3333",
+// 	"content": "4444"
+// }
 
 
 function Signup() {
@@ -40,7 +76,7 @@ function Signup() {
 
 
     const currentPostData = useSelector(state => state.currentPostData);
-  
+
 
     const alerts = useSelector(state => state.alerts);
 
@@ -51,15 +87,15 @@ function Signup() {
     const focusedPostObject = useSelector(state => state.focusedPostObject);
 
     useEffect(() => {
-      // Função executada toda vez que "focusedPostObject" for alterado
-      if(focusedPostObject ===null)return
-      console.log('focusedPostObject alterado:', focusedPostObject);
-      setTitleText(focusedPostObject.title)
-      setContentText(focusedPostObject.content)
-      // Realize qualquer ação necessária com o novo valor de "focusedPostObject"
+        // Função executada toda vez que "focusedPostObject" for alterado
+        if (focusedPostObject === null) return
+        console.log('focusedPostObject alterado:', focusedPostObject);
+        setTitleText(focusedPostObject.title)
+        setContentText(focusedPostObject.content)
+        // Realize qualquer ação necessária com o novo valor de "focusedPostObject"
     }, [focusedPostObject]);
 
-  
+
 
 
     function handleTextContent(text) {
@@ -74,7 +110,7 @@ function Signup() {
     useEffect(() => {
         setTitleText("")
         setContentText("")
-        
+
     }, [])
 
 
@@ -144,7 +180,7 @@ function Signup() {
             store.dispatch({ type: "LOAD_NEXT_POSTS" })
         else if (direction == "previous")
             store.dispatch({ type: "LOAD_PREVIOUS_POSTS" })
-            forceScrollToTop()
+        forceScrollToTop()
     }
 
 
@@ -158,31 +194,31 @@ function Signup() {
 
     function forceScrollToTop() {
         let element = document.body.childNodes[1] // the scroll itself
-        if(element) {
+        if (element) {
             element.scrollTop = 0
         }
-        
+
     }
 
 
     let scrollcards = <div>
-    {
-        currentPostData != null &&
-        currentPostData.results.map((element) => {
-            console.log(element.id)
-            return <PostCard key={element.id} obj_id={element.id} username={element.username} message={element.content} title={element.title} date={formatElapsedTime(element.created_datetime)} editable={state.user == element.username} />
-        }) ||
-        <>
-            <img className={style["loading-icon"]} src={loading_icon}></img>
-            <b className={style["loading-text"]}>LOADING POSTS</b>
-        </>
+        {
+            currentPostData != null &&
+            currentPostData.results.map((element) => {
+                console.log(element.id)
+                return <PostCard key={element.id} obj_id={element.id} username={element.username} message={element.content} title={element.title} date={formatElapsedTime(element.created_datetime)} editable={state.user == element.username} />
+            }) ||
+            <>
+                <img className={style["loading-icon"]} src={loading_icon}></img>
+                <b className={style["loading-text"]}>LOADING POSTS</b>
+            </>
 
-    }
-
- 
+        }
 
 
-</div>
+
+
+    </div>
 
 
     return <div className={style["outer-component"]}>
@@ -192,13 +228,13 @@ function Signup() {
                     CodeLeap Network
                 </h1>
 
-                <svg onClick={userIconClick} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" style={{cursor: "pointer"}}  viewBox="0 0 16 16">
+                <svg onClick={userIconClick} xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" style={{ cursor: "pointer" }} viewBox="0 0 16 16">
                     <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
                     <path fillRule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z" />
                 </svg>
             </header>
             <MindCard />
-            
+
             {
                 alerts.delete_visible && (
                     <AlertBox message="Are you sure you want to delete this item?">
@@ -246,27 +282,27 @@ function Signup() {
             }
 
             {currentPostData != null &&
-              <div className={style["pagination"]}>
-              <button
-                onClick={() => loadPostss("previous")}
-                style={{
-                  color: currentPostData.previous ? "#7695EC" : "gray",
-                  pointerEvents: currentPostData.previous ? "auto" : "none",
-                }}
-              >
-                PREVIOUS PAGE
-              </button>
-              <button
-                onClick={() => loadPostss("next")}
-                style={{
-                  color: currentPostData.next ? "#7695EC" : "gray",
-                  pointerEvents: currentPostData.next ? "auto" : "none",
-                }}
-              >
-                NEXT PAGE
-              </button>
-            </div>
-            
+                <div className={style["pagination"]}>
+                    <button
+                        onClick={() => loadPostss("previous")}
+                        style={{
+                            color: currentPostData.previous ? "#7695EC" : "gray",
+                            pointerEvents: currentPostData.previous ? "auto" : "none",
+                        }}
+                    >
+                        PREVIOUS PAGE
+                    </button>
+                    <button
+                        onClick={() => loadPostss("next")}
+                        style={{
+                            color: currentPostData.next ? "#7695EC" : "gray",
+                            pointerEvents: currentPostData.next ? "auto" : "none",
+                        }}
+                    >
+                        NEXT PAGE
+                    </button>
+                </div>
+
             }
 
             {
@@ -276,22 +312,22 @@ function Signup() {
 
 
 
-                    <div style={{  width: "100%"} }>
-                        <div style={{overflow: "scroll"}}>
-                        <b>username </b> {username}
-                            </div> 
-                      
-                    <div style={{ display: "flex", gap: "16px", width: "100%", marginLeft:"auto" }}>
-                        <button onClick={logout} className="button-to-right" style={{ backgroundColor: "#FF5151"}}>
-                            <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
-                                <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
-                                <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
-                            </svg>
-
-                            LOGOUT</button>
-                        <button onClick={handleCloseUserAlert}>CLOSE</button>
+                    <div style={{ width: "100%" }}>
+                        <div style={{ overflow: "scroll" }}>
+                            <b>username </b> {username}
                         </div>
-                        
+
+                        <div style={{ display: "flex", gap: "16px", width: "100%", marginLeft: "auto" }}>
+                            <button onClick={logout} className="button-to-right" style={{ backgroundColor: "#FF5151" }}>
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" viewBox="0 0 16 16">
+                                    <path fillRule="evenodd" d="M6 12.5a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5v-9a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5v2a.5.5 0 0 1-1 0v-2A1.5 1.5 0 0 1 6.5 2h8A1.5 1.5 0 0 1 16 3.5v9a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 5 12.5v-2a.5.5 0 0 1 1 0v2z" />
+                                    <path fillRule="evenodd" d="M.146 8.354a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L1.707 7.5H10.5a.5.5 0 0 1 0 1H1.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3z" />
+                                </svg>
+
+                                LOGOUT</button>
+                            <button onClick={handleCloseUserAlert}>CLOSE</button>
+                        </div>
+
 
 
                     </div>
